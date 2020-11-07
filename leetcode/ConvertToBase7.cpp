10591f33f89d34a2690c12a1d654ca4e53c319b8
Levon <deimusmeister@hotmail.com>
2020-11-07 13:56:06 +0100

    Create ConvertToBase7.cpp

---
+++ leetcode/ConvertToBase7.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/base-7/
+// 
+// Given an integer, return its base 7 string representation.
+// 
+// Example 1:
+// Input: 100
+// Output: "202"
+// Example 2:
+// Input: -7
+// Output: "-10"
+// Note: The input will be in range of [-1e7, 1e7].
+
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num == 0) {
+            return "0";
+        }
+        std::string result = "";
+        bool neg = false;
+        if(num < 0) {
+            neg = true;
+            num = std::abs(num);
+        }
+        while(num != 0) {
+            result += std::to_string(num % 7);
+            num = num / 7;
+        }
+        if(neg) {
+            result += '-';
+        }
+        std::string val(result.rbegin(), result.rend());
+        return val;
+    }
+};
