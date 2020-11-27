09dbc25e4b1e0da43dd89e7320684f19c300b7be
Levon <deimusmeister@hotmail.com>
2020-11-27 20:36:26 +0100

    Create AllPathsSourceTarget.cpp

---
+++ leetcode/medium/AllPathsSourceTarget.cpp
@@ -0,0 +1,63 @@
+// https://leetcode.com/problems/all-paths-from-source-to-target/
+// 
+// Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1, and return them in any order.
+// 
+// The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+// 
+//  
+// 
+// Example 1:
+// 
+// 
+// Input: graph = [[1,2],[3],[3],[]]
+// Output: [[0,1,3],[0,2,3]]
+// Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+// Example 2:
+// 
+// 
+// Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+// Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+// Example 3:
+// 
+// Input: graph = [[1],[]]
+// Output: [[0,1]]
+// Example 4:
+// 
+// Input: graph = [[1,2,3],[2],[3],[]]
+// Output: [[0,1,2,3],[0,2,3],[0,3]]
+// Example 5:
+// 
+// Input: graph = [[1,3],[2],[3],[]]
+// Output: [[0,1,2,3],[0,3]]
+//  
+// 
+// Constraints:
+// 
+// n == graph.length
+// 2 <= n <= 15
+// 0 <= graph[i][j] < n
+// graph[i][j] != i (i.e., there will be no self-loops).
+// The input graph is guaranteed to be a DAG.
+
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        std::vector<int> path{0};
+        int n = graph.size() ;
+        visit(graph, path, 0, n - 1);
+        return result;
+    }
+    void visit(vector<vector<int>>& graph, std::vector<int> path, int cur, int target) {
+        if(cur == target) {
+            result.push_back(path);
+            return;
+        }
+        for(auto i : graph[cur]) {
+            std::vector<int> local(path);
+            local.push_back(i);
+            visit(graph, local, i, target);
+        }
+    }
+private:
+    std::vector<std::vector<int>> result;
+};
